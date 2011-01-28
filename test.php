a6bb47589ac0242ea9a41228bde4ab750591f877
Robert Agthe <robert@scriptshit.de>
2011-01-28 17:09:14 +0100

    Aufgräumt: Unnützen Kram entfernt. Haleluja
    Signed-off-by: Robert Agthe <robert@scriptshit.de>

--- test.php
+++
@@ -1,6 +0,0 @@
-<?
-
-	$data1 = 'Ich habe mal die endgültigen Ergebnisse öffentlich gemacht. Danke fürs mitmachen Leute :D';
-	$data2 = 'Ich habe mal die endgültigen Ergebnisse öffentlich gemacht. Danke fürs mitmachen Leute :D #fb hallo';
-	similar_text($data1,$data2,$prozent);
-	echo $prozent;
